f78a0a13502358264e413387f885be2d7dd902b9
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-13 18:27:26 -0600

    Cleaned up code and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 const apiKey = "8a73232fcd06372324159654883c0590";
 
 
+// When the page first loads, start message is displayed
+$().ready(function () {
+    $('#start-message').removeClass('hide').append('<h2> Enter city name above to view <br/> current weather & five-day forecast <h2>');
+}) 
+
 
 // ----- When user clicks the search button-------
 $(".search-button").on("click", function (event) {
+
     event.preventDefault();
 
-    $('#error-message').addClass('hide'); //hides error message if displayed
+    $('#error-message').empty().addClass('hide'); //hides error message if displayed and empties it
+    $('#start-message').empty().addClass('hide'); //hides landing page message
 
     var userInput = $(this).siblings("input").val();
     var storedSearchItems = JSON.parse(localStorage.getItem("searchItems"));
@@ -22,31 +29,26 @@ $(".search-button").on("click", function (event) {
             storedSearchItems.push(userInput);
         }
         
-        getWeather(userInput);
+        getWeather(userInput); //function retrieves weather data and renders it on page
 
-        // Puts user input into local storage
-        localStorage.setItem("searchItems", JSON.stringify(storedSearchItems));
+        localStorage.setItem("searchItems", JSON.stringify(storedSearchItems)); // Puts user input into local storage
 
-        // Adds the most recent search item (last in array) to the "Search Again:" line on the page
-        var searchedItemCount = $('#search-history button').length;
-        console.log(searchedItemCount);
+        var searchedItemCount = $('#search-history button').length; // Adds the most recent search item (last in array) to search history
 
-        if (searchedItemCount < 6) {
+        // limits the number of searched items displayed on page to 6 items
+        if (searchedItemCount < 7) {
             $('#search-history').prepend($("<button> " + storedSearchItems[storedSearchItems.length - 1] + " </button>")).removeClass('hide');
             $('#search-history').children('button').addClass('searched-item');
         }
 
-        // Deletes a previously searched item from the page
+        // Deletes a previously searched item from the page to keep item total at 6
         else {
             $('#search-history').prepend($("<button> " + storedSearchItems[storedSearchItems.length - 1] + " </button>")).removeClass('hide');
             $('#search-history').children('button').addClass('searched-item');
             $('#search-history').children().last().remove();
         }
 
-
-
-        // Clears the search box after search button is clicked
-        $("input").val(""); 
+        $("input").val(""); // Clears the search box after search button is clicked
     }
 
     // If search button is clicked, but there's nothing typed into the search bar, nothing happens
@@ -56,32 +58,27 @@ $(".search-button").on("click", function (event) {
 });
 
 
-
-
-
-// ----------When user clicks an city previously searched--------------------
+// ----------When user clicks a city previously searched--------------------
 $("#search-history").on("click", function (event) {
 
     event.preventDefault();
 
-    $('#error-message').addClass('hide'); //hides error message if displayed
+    $('#error-message').empty().addClass('hide'); //hides error message if displayed and empties it
 
     var target = $( event.target );
 
-    if (target.is('.searched-item')) {
+    if (target.is('.searched-item')) { //handles the click
 
         pastUserInput = target.text();
         console.log(pastUserInput);
     }
 
-    getWeather(pastUserInput);
-
+    getWeather(pastUserInput); //function retrieves weather data and renders it on page
 });
 
 
-
-
-// This function uses fetch two times for two different URLs
+// This function retrieves weather data from APIs and renders it on page
+// uses fetch two times for two different URLs
 function getWeather (userInput) {
 
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&appid=' + apiKey;
@@ -89,13 +86,12 @@ function getWeather (userInput) {
     // First fetch
     fetch(apiUrl)
 
-        .then (function (response) {
-            // Turns the response into a JavaScript Object
-            return response.json() 
+        .then (function (response) {  
+            return response.json() // Turns the response into a JavaScript Object to be used in next .then block
         })
 
         .then(function (data) {
-            console.log(data);
+            console.log("FIRST API CALL >>>>", data);
 
             // variables for current weather
             var cityName = data.name;
@@ -107,7 +103,7 @@ function getWeather (userInput) {
             var longitude = data.coord.lon; //need this data to perform the second fetch
 
             // Current weather icon
-            var currentWeatherIcon = $("<img>")
+            var currentWeatherIcon = $("<img>") //create new element
             currentWeatherIcon.attr("src" , "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
             $('.current-weather-icon').empty();
             $('.current-weather-icon').append(currentWeatherIcon);
@@ -129,16 +125,15 @@ function getWeather (userInput) {
             })
         
         .then (function (response) {
-            return response.json()
+            return response.json() //Turns reponse into a JavaScript object to be used in next .then block
         })
 
         .then (function (data) {
-            console.log(data);
+            console.log("SECOND API CALL >>>>", data);
 
             var uvIndex = data.current.uvi;
-
-            // creates a new <span> element that will contain the uv-index number
-            $('.uv-index').html("UV Index: ").append('<span></span>');
+            
+            $('.uv-index').html("UV Index: ").append('<span></span>'); // creates a new <span> element that will contain the uv-index number
 
             setUvIndexColor(uvIndex); //function changes background color of uv-index
 
@@ -146,18 +141,16 @@ function getWeather (userInput) {
 
         })
 
+        // If user types invalid city, error message displays
         .catch (function (error){
             $('.weather-result').addClass('hide');
             $('#search-history').children().first().remove();
             $('#error-message').empty()
             .append("<h3> Unable to find city. Please check for spelling errors and try again.</h3>")
             .removeClass('hide');
-            // $('main').prepend("<h3> Unable to find city. Please check for spelling errors and try again.</h3>")
             return;
     })   
 };
-
-
 
 
 // The background color of the UV-Index changes based on severity
@@ -185,7 +178,6 @@ function setUvIndexColor (uvIndex) {
 };
 
 
-
 // Creates the five day forecast
 function renderForecast (data) {
 
@@ -194,13 +186,13 @@ function renderForecast (data) {
     var index = 0
 
 
-    dateLine.each(function () { //displays the date, temp, and humidity for each blue box
+    dateLine.each(function () { //displays the date, icon, temp, and humidity for each blue box
         
         var forecastDate = todayDate.add(1, 'days').format("M/D/YYYY");
         var forecastTemp = Math.floor((data.daily[index].temp.max));
         var forecastHumidity = (data.daily[index].humidity);
 
-        var forecastIcon = $("<img>")
+        var forecastIcon = $("<img>") //creates new element
         forecastIcon.attr("src" , "https://openweathermap.org/img/w/" + data.daily[index].weather[0].icon + ".png");
 
         $(this).html(forecastDate)
@@ -209,6 +201,5 @@ function renderForecast (data) {
         .siblings('.forecast-icon').empty().append(forecastIcon);
 
         index ++;
-
     });
 };
